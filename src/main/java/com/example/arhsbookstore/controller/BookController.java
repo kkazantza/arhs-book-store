af4a8263bb755cf341a662531e5f9079625ca044
kazantka <akazantza@gmail.com>
2021-06-10 09:47:02 +0300

    controller, service, repository

--- src/main/java/com/example/arhsbookstore/controller/BookController.java
+++ src/main/java/com/example/arhsbookstore/controller/BookController.java
@@ -1,2 +1,29 @@
-package com.example.arhsbookstore.controller;public class BookController {
+package com.example.arhsbookstore.controller;
+
+import com.example.arhsbookstore.model.Book;
+import com.example.arhsbookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/arhs-book-store")
+public class BookController {
+
+    @Autowired
+    BookService bookService;
+
+    @GetMapping("/books")
+    private List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping("/book")
+    private Book saveBook(Book book) {
+        return bookService.saveOrUpdate(book);
+    }
+
+
 }
--- src/main/java/com/example/arhsbookstore/service/BookService.java
+++ src/main/java/com/example/arhsbookstore/service/BookService.java
@@ -1,2 +1,25 @@
-package com.example.arhsbookstore.service;public class BookService {
+package com.example.arhsbookstore.service;
+
+import com.example.arhsbookstore.model.Book;
+import com.example.arhsbookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Book saveOrUpdate(Book book) {
+        return bookRepository.save(book);
+    }
 }
